68c883238935e76ade2294d91049d76a5711c9a8
TaylanPostalci <tay.postal@gmail.com>
2022-08-12 23:04:09 -0400

    arraylab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(Broom){
+ return [...cats,"Broom"]
+}
+function prependCat(Arnold){
+    return ["Arnold",...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat(){
+    return cats.slice(1,cats.length)
+}
